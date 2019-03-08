a7c516be1eaf092758ce1816951234364455a06a
Jun Chen <junchen07@gmail.com>
2019-03-07 19:20:37 -0500

    trying to deloy

---
+++ precache-manifest.1f0ba1ba5528aefd92c79cb24daec47b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "cdf2e6fbae3412b8a7ed",
+    "url": "/static/js/main.cdf2e6fb.chunk.js"
+  },
+  {
+    "revision": "0401b3075cf74fbb2d88",
+    "url": "/static/js/2.0401b307.chunk.js"
+  },
+  {
+    "revision": "cdf2e6fbae3412b8a7ed",
+    "url": "/static/css/main.232e8908.chunk.css"
+  },
+  {
+    "revision": "0401b3075cf74fbb2d88",
+    "url": "/static/css/2.67f603be.chunk.css"
+  },
+  {
+    "revision": "8c2dc3be9a8c694aa59e891719936e23",
+    "url": "/index.html"
+  }
+];
